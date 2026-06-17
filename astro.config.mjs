ee99368c4745d62b62f44126504fd32d42d29bb4
Mark Lawson <mark@walkonmars.co.uk>
2026-06-17 15:00:24 +0100

    Set up Astro with SSR, Node adapter, and Sanity/React integrations for Visual Editing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,32 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import node from "@astrojs/node";
+import sanity from "@sanity/astro";
+import react from "@astrojs/react";
+import { loadEnv } from "vite";
+
+const { PUBLIC_SANITY_PROJECT_ID, PUBLIC_SANITY_DATASET } = loadEnv(
+  process.env.NODE_ENV ?? "development",
+  process.cwd(),
+  "",
+);
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "server",
+  adapter: node({
+    mode: "standalone",
+  }),
+  integrations: [
+    sanity({
+      projectId: PUBLIC_SANITY_PROJECT_ID,
+      dataset: PUBLIC_SANITY_DATASET,
+      useCdn: false,
+      apiVersion: "2026-06-17",
+      stega: {
+        studioUrl: "http://localhost:3333",
+      },
+    }),
+    react(),
+  ],
+});
